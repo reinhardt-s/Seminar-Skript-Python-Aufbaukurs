fc9b6ca1fdd1b616f2007aa2fa3ec8a89873e96e
reinhardt <s_reinhardt@outlook.de>
2023-06-19 16:10:59 +0200

    Update 01_order_66.py

--- Kapitel 5 - Arbeiten mit regulären Ausdrücken /challenge/01_order_66.py
+++ Kapitel 5 - Arbeiten mit regulären Ausdrücken /challenge/01_order_66.py
@@ -3,3 +3,8 @@
 # Zerlege die Bestellungen mit regulären Ausdrücken in ihre Bestandteile.
 # Lade die Bestellungen in eine Liste von Order-Objekten.
 # Gebe die Bestellungen auf der Konsole aus.
+
+orders_from_string = [
+    'Han Solo, Smuggler, 1x Hyperdrive Generator, 3x Droid Repair Kit, 2x Beskar Steel',
+    'Luke Skywalker, Jedi, 1x Lightsaber, 4x Droid Repair Kit, 2x Holocron'
+]
